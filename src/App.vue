571fcc6ebf5800e52f891cc47a453ef999f31130
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-23 22:32:06 +0100

    Feat: using types within the data property

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app">
-    <p>Hello! Welcome to Vue.js with TypeScript</p>
+    <p> {{ name }} - {{ age }}</p>
+    <button @click="changeName('Akin')">
+      Change name
+    </button>
+    <button @click="changeAge('32')">
+      Change name
+    </button>
   </div>
 </template>
 
@@ -9,7 +15,24 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {},
+
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string // This way we can provide two types for the age variable within the data block
+    }
+  },
+
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+
+    changeAge(age: number | string) {
+      this.age = age;
+    }
+  }
 });
 </script>
 
